6906ecbe8fc8ed6168e432bec405cab7309e4e22
LucasLopez1 <lucaslopez@udesa.edu.ar>
2026-06-04 17:11:01 -0300

    Hice el register, le falta el estilo

--- src/screens/register/Register.js
+++ src/screens/register/Register.js
@@ -1,9 +1,55 @@
-import { View, Text } from 'react-native';
+import {useState} from 'react';
+import { View, Text, Pressable, TextInput } from 'react-native';
+import { auth, db } from '../firebase/config';
 
-function Register(){
+function Register(props){
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  function onSubmit(){
+    auth.createUserWithEmailAndPassword(email, password)
+      .then( response => {
+        props.navigation.navigate('Login')
+        db.collection('users').add({
+          email: email,
+          username: username,
+          createdAt: Date.now(),
+        })
+          .then()
+          .catch( e => console.log(e))
+      })
+      .catch( error => {
+        setError('Fallo en el registro.')
+      })
+  }
+
   return (
     <View>
-      <Text>Register</Text>
+      <TextInput
+        keyboardType='email-address'
+        placeholder='Email'
+        onChangeText={ text => setEmail(text) }
+        value={email} />
+      <TextInput
+        keyboardType='default'
+        placeholder='Username'
+        onChangeText={ text => setUsername(text) }
+        value={username} />
+      <TextInput
+        keyboardType='default'
+        secureTextEntry={true}
+        placeholder='Password'
+        onChangeText={ text => setPassword(text) }
+        value={password} />
+      <Pressable onPress={() => onSubmit()}>
+        <Text> Registrate </Text>
+      </Pressable>
+      <Text>{error}</Text>
+      <Pressable onPress={() => props.navigation.navigate('Login')}>
+        <Text>Ya tenés cuenta? Iniciá sesión</Text>
+      </Pressable>
     </View>
   );
 }
