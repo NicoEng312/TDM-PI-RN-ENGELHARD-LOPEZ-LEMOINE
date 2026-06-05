d728568f306848bcd0a6644426aacdec74b64cda
LucasLopez1 <lucaslopez@udesa.edu.ar>
2026-06-05 15:47:21 -0300

    Armo el MenuStack para login y register, falta todo el resto

--- src/navigation/menustack/MenuStack.js
+++ src/navigation/menustack/MenuStack.js
@@ -1,10 +1,19 @@
-import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Login from '../../screens/login/Login';
+import Register from '../../screens/register/Register';
+
+const Stack = createNativeStackNavigator();
 
 function MenuStack(){
   return (
-    <View>
-      <Text>MenuStack</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={ Login } />
+        <Stack.Screen name="Register" component={ Register } />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
