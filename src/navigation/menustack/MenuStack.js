a2edcf588df585b2858e1d3604e926172b101bf9
NicoEng312 <nengelhard@udesa.edu.ar>
2026-06-08 17:48:28 -0300

    hice el tab

--- src/navigation/menustack/MenuStack.js
+++ src/navigation/menustack/MenuStack.js
@@ -1,18 +1,34 @@
+import { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Login from '../../screens/login/Login';
 import Register from '../../screens/register/Register';
+import MenuTab from '../menutab/MenuTab';
+
+import { auth } from '../../firebase/config';
 
 const Stack = createNativeStackNavigator();
 
-function MenuStack(){
+function MenuStack() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    auth.onAuthStateChanged(userFromFirebase => {
+      setUser(userFromFirebase);
+    });
+  }, []);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={ Login } />
-        <Stack.Screen name="Register" component={ Register } />
-      </Stack.Navigator>
+      {user ? (
+        <MenuTab />
+      ) : (
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Register" component={Register} />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 }
--- src/navigation/menutab/MenuTab.js
+++ src/navigation/menutab/MenuTab.js
@@ -1,10 +1,44 @@
-import { View, Text } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function MenuTab(){
+import Home from '../../screens/home/Home';
+import CrearPost from '../../screens/crearpost/CrearPost';
+import MiPerfil from '../../screens/miperfil/MiPerfil';
+import Comentarios from '../../screens/comentarios/Comentarios';
+
+const Tab = createBottomTabNavigator();
+const HomeStack = createNativeStackNavigator();
+
+function HomeStackNavigator() {
   return (
-    <View>
-      <Text>MenuTab</Text>
-    </View>
+    <HomeStack.Navigator screenOptions={{ headerShown: false }}>
+      <HomeStack.Screen name="HomeScreen" component={Home} />
+      <HomeStack.Screen name="Comentarios" component={Comentarios} />
+    </HomeStack.Navigator>
+  );
+}
+
+function MenuTab() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+          if (route.name === 'Inicio') iconName = 'home-sharp';
+          else if (route.name === 'Crear post') iconName = 'add';
+          else if (route.name === 'Mi perfil') iconName = 'person-circle-sharp';
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#5C67F2',
+        tabBarInactiveTintColor: '#999',
+      })}
+    >
+      <Tab.Screen name="Inicio" component={HomeStackNavigator} />
+      <Tab.Screen name="Crear post" component={CrearPost} />
+      <Tab.Screen name="Mi perfil" component={MiPerfil} />
+    </Tab.Navigator>
   );
 }
 
