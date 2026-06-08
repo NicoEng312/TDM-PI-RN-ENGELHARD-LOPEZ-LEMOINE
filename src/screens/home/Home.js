9bee9606d1cb7f8360afa053e7a3aa91c705e727
bautilemoine <blemoine@udesa.edu.ar>
2026-06-08 19:28:52 -0300

    home,js

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,9 +1,42 @@
-import { View, Text } from 'react-native';
+import { useState, useEffect } from 'react';
+import { View, FlatList, ActivityIndicator, StyleSheet } from 'react-native';
+import { db } from '../../firebase/config';
+import Post from '../../components/post/Post';
 
-function Home(){
+function Home(props) {
+  const [posteos, setPosteos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    db.collection('posts')
+      .orderBy('createdAt', 'desc')
+      .onSnapshot(docs => {
+        let posts = [];
+        docs.forEach(doc => {
+          posts.push({
+            id: doc.id,
+            data: doc.data(),
+          });
+        });
+        setPosteos(posts);
+        setLoading(false);
+      });
+  }, []);
+
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      {loading ? (
+        <ActivityIndicator size="large" color="#5C67F2" />
+      ) : (
+        <FlatList
+          style={styles.flatlist}
+          data={posteos}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <Post post={item} navigation={props.navigation} />
+          )}
+        />
+      )}
     </View>
   );
 }
