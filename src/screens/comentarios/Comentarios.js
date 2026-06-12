0cc35cd46801ece3742d0026a8977e03888ed6d6
bautilemoine <blemoine@udesa.edu.ar>
2026-06-11 22:24:29 -0300

    css

--- src/screens/comentarios/Comentarios.js
+++ src/screens/comentarios/Comentarios.js
@@ -74,3 +74,62 @@ function Comentarios(props) {
 }
 
 export default Comentarios;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 16,
+  },
+  titulo: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#222',
+    marginBottom: 12,
+  },
+  formulario: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 8,
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+    fontSize: 14,
+    marginRight: 8,
+  },
+  boton: {
+    backgroundColor: '#5C67F2',
+    borderRadius: 8,
+    paddingVertical: 10,
+    paddingHorizontal: 16,
+  },
+  textoBoton: {
+    color: '#fff',
+    fontWeight: '600',
+  },
+  lista: {
+    width: '100%',
+    flex: 1,
+  },
+  comentario: {
+    borderBottomWidth: 1,
+    borderColor: '#eee',
+    paddingVertical: 10,
+  },
+  autor: {
+    fontWeight: 'bold',
+    fontSize: 14,
+    color: '#222',
+    marginBottom: 2,
+  },
+  texto: {
+    fontSize: 14,
+    color: '#333',
+  },
+});
+
+export default Comentarios;
